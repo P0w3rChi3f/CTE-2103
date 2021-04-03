246f262709183aa6ccf576b528e1f41765e5a0f5
James Honeycutt <honeycutt.james@gmail.com>
2021-04-03 12:24:59 -0400

    Working on reading the file

--- Scripts/Python/listener.py
+++ Scripts/Python/listener.py
@@ -1,11 +1,16 @@
 import socket
-with socket.socket() as s:
-     s.bind(('',9999))
-     s.listen()
-     conection, ipaddr = s.accept()
 
-     with connection:
-
-         print("Connection from", ipaddr)
-         while True:
-         connection.sendall(b"220 ProFTPB 1.3.5 Server (Great Job!) [172.17.0.2]\n")
+def myListener():
+    with socket.socket() as s:
+        s.bind(('',9999))
+        s.listen()
+        conn, ipaddr = s.accept()
+        with conn:
+            print('Connected by', ipaddr)
+            while True:
+                conn.sendall(b"220 ProFTPB 1.3.5 Server (Great Job!) [172.17.0.2]\n")
+                conn.recv(1024)    
+        
+        
+        
+        #
--- Scripts/Python/sockets.py
+++ Scripts/Python/sockets.py
@@ -1,4 +1,6 @@
 import socket
+import os
+
 with socket.socket() as s:
     s.connect(('192.168.229.105', 21))
     print(s.recv(4096))
@@ -7,3 +9,24 @@ with socket.socket() as s:
 with socket.socket() as s:
     s.connect(('192.168.229.105', 21))
     print(s.recv(4096).decode(ascii).lstrip('220 '))
+
+# Settings for my own listner
+def myGrabber():
+    with socket.socket() as s:
+        s.connect(('127.0.0.1', 9999))
+        banner = s.recv(17).decode('ascii').lstrip('220 ')
+        print(banner)
+        
+### Still need work
+with open("/Users/Jay/Documents/GitHub-Repositories/Notes/CTE-2103/Scripts/Python/artofwar.txt") as handle:
+    handle.readline()
+    text = 'ProFTPB 1.3.5'
+    linenum = 0
+    for line in handle:
+        if text in line:
+            print('I found the text')
+            print('The full line reads as:')
+            print(line)
+        else:
+            handle.readline()
+
